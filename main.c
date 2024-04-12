e175a06cb36e93cfee1657068124410f83de9e82
thiagorabaioli <tfrabaioli@outlook.com>
2024-04-12 22:00:31 +0100

    Example search string

--- main.c
+++ main.c
@@ -1,19 +1,29 @@
 #include <stdio.h>
 
-int Troca(int *x, int *y){
-    static int trocas =0;
-    int aux=*x;
-     *x=*y;
-     *y=aux;
-     return ++trocas;
-}
+char *strstr(char *find, char *str){
+    int i;
+    /*Percorrer a String*/
+    while (*str !=0) {
+        /*Tenta fazer o match a começar em str*/
+
+        for (i=0; str[i]!=0 && find[i]!=0 && find[i]==str[i]; i++);
+        /*Se chegou ao fim da string de procura, não há match*/
+        if (find[i] == 0)
+            return str;
+        str++;
+    }
+
+      return NULL;
+    }
+
 
 
 int main() {
-    int x=123, y=321;
-    printf("Troca %d: ", Troca(&x,&y)); printf("x: %d y: %d\n", x,y);
-    printf("Troca %d: ", Troca(&x,&y)); printf("x: %d y: %d\n", x,y);
-    printf("Troca %d: ", Troca(&x,&y)); printf("x: %d y: %d\n", x,y);
-    printf("Troca %d: ", Troca(&x,&y)); printf("x: %d y: %d\n", x,y);
-    return 0;
+    char str[]="percorrer toda a string str";
+    char find[10];
+    char *resultado;
+    printf("Introduza a string que procura: ");
+    gets(find);
+    resultado= strstr(find, str);
+    printf("Resutado: %s\n", resultado);
 }
