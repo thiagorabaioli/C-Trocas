e53692622af921bb0e34f720b9e8393d8118e71a
thiagorabaioli <tfrabaioli@outlook.com>
2024-04-12 21:34:41 +0100

    example exchang and static variable for count numbers of changes

--- main.c
+++ main.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 
-void Troca(int *x, int *y){
+int Troca(int *x, int *y){
+    static int trocas =0;
     int aux=*x;
      *x=*y;
      *y=aux;
-
+     return ++trocas;
 }
 
 
 int main() {
     int x=123, y=321;
-    Troca(&x,&y);
-    printf("x: %d y: %d", x,y);
+    printf("Troca %d: ", Troca(&x,&y)); printf("x: %d y: %d\n", x,y);
+    printf("Troca %d: ", Troca(&x,&y)); printf("x: %d y: %d\n", x,y);
+    printf("Troca %d: ", Troca(&x,&y)); printf("x: %d y: %d\n", x,y);
+    printf("Troca %d: ", Troca(&x,&y)); printf("x: %d y: %d\n", x,y);
     return 0;
 }
